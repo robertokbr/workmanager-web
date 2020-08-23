4eb486d55d11d02c854c2c2749116ec5c38ff976
Roberto Junior <robertojuniordev@gmail.com>
2020-08-22 23:59:49 -0300

    mvp 2

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-vp  baseURL: 'https://testeapiintranett.herokuapp.com/task',
+  baseURL: process.env.REACT_APP_API_URL,
 });
 
 export default api;
