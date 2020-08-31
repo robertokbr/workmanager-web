375b1c9601721da4068608ebcd1e793133f106f5
robertokbr <robertojuniordev@gmail.com>
2020-08-31 19:25:57 -0300

    Remove Lint errors

--- src/pages/TeamUserTask/index.tsx
+++ src/pages/TeamUserTask/index.tsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import { FiArrowLeft, FiArchive } from 'react-icons/fi';
+import { FiArrowLeft } from 'react-icons/fi';
 import { Link, useRouteMatch } from 'react-router-dom';
 
 import api from '../../services/api';
