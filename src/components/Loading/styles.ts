8056a1aff4e8c293702de1ce706115b382acb749
robertokbr <robertojuniordev@gmail.com>
2020-08-31 17:43:54 -0300

    Add loading page component

---
+++ src/components/Loading/styles.ts
@@ -0,0 +1,9 @@
+import styled from 'styled-components';
+
+export const Container = styled.div`
+  display: flex;
+  flex: 1;
+  background: #121212ff;
+  align-items: center;
+  justify-content: center;
+`;
